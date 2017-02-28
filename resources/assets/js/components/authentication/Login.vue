c354b5faeeaa568e53cb08c7d8336c90d1b29e2d
Fredrick W. Warren <fred.warren@gmail.com>
2017-02-27 21:27:24 -0800

    #06 Authentication & Devtools

---
+++ resources/assets/js/components/authentication/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="row">
+        <div class="col-md-6 col-md-offest3">
+            <div class="panel panel-defautl">
+                <div class="panel-body">
+                    <div class="forms-group">
+                        <input v-model="email" class="form-control" type="email" placeholder="Email">
+                    </div>
+
+                    <div class="forms-group">
+                        <input v-model="password" class="form-control" type="password" placeholder="Password">
+                    </div>
+
+                   <button class="btn btn-success pull-right">Login</button>
+               </div>
+           <div>
+        <div>
+    <div>
+</template>
+
+
+<script>
+    export defualt {
+        data () {
+            return {
+                email: '',
+                password: ''
+            }
+        }
+    }
+<script>
+
+<style>
+
+</style>
+
